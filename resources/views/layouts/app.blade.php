77fd3bcea72ba57f7d5302d70a25eb7ec99ac629
taro.kirameki <mako.dora0509@gmai.com>
2020-08-30 11:32:59 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,9 @@
             </nav>
         </header>
 
+{{-- ナビゲーションバー --}}
+        @include('commons.navbar')
+
         <div class="container">
              {{-- エラーメッセージ --}}
             @include('commons.error_messages')
